e2abc6da57dd9bbdf9d018194eee5f59f8ec8315
rocioparra <rparra@itba.edu.ar>
2018-09-16 16:13:43 -0300

    -

---
+++ Ej2/matlab/e1_tp1_ej2.m
@@ -0,0 +1,7 @@
+w = 2*pi*logspace(2,5,1000);
+
+%superponedor('','e1_tp1_ej2_hf.csv','e1_tp1_ej2_hf_spice.csv',w, 'e1_tp1_ej2_hf');
+
+%superponedor('','e1_tp1_ej2_zin.csv','e1_tp1_ej2_zin_spice.csv',w, 'e1_tp1_ej2_zin');
+
+superponedor('','e1_tp1_ej2_zout.csv','e1_tp1_ej2_zout_spice.csv',w, 'e1_tp1_ej2_zout');
